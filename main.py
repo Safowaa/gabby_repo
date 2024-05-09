00e2b811ce01e60afbfccf77e3deb3d6d1e75fb9
Safowaa <safowaabenedicta@gmail.com>
2024-05-09 12:55:52 +0000

    Add addition method calculator

---
+++ main.py
@@ -0,0 +1,14 @@
+class calculator():
+    def add(self, x, y):
+        return x + y
+
+    # def subtract(self, x, y):
+
+    #     return x - y
+
+    # def multiply(self, x, y):
+    #     return x * y
+
+    # def divide(self, x, y):
+
+    #     return x / y
