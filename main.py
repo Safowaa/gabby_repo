a4228bb829e08450ef902ef1c1d25670aa6d58ca
Safowaa <safowaabenedicta@gmail.com>
2024-05-09 13:41:03 +0000

    add multiplication method to the calculater

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ class calculator():
 
         return x - y
 
-    # def multiply(self, x, y):
-    #     return x * y
+    def multiply(self, x, y):
+        return x * y
 
     # def divide(self, x, y):
 
